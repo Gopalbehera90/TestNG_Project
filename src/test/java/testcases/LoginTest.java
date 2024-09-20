f618cf25badb3532107478358d3e1c2ddd776fcf
Gopalbehera90 <gopalbehera90@gmail.com>
2024-09-20 11:35:30 +0530

    My TestNG Project

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,38 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginTest extends BaseClass {
+	
+	
+	
+	@Test
+	public void LoginFailiureTest() {
+		
+		LoginPage login = new LoginPage(driver);
+		login.LoginFunction("abc@xyz.com","Anc@1234");
+		
+		WebElement errorMessage = driver.findElement(By.className("error_msg"));
+		System.out.println("The error message is " + errorMessage.getText());
+		
+		String actMsg = errorMessage.getText();
+		String expMsg = "The email or password you have entered is invalid.";
+		AssertJUnit.assertEquals(actMsg,expMsg);
+		
+		
+	}
+	
+	@Test
+	public void LoginSuccessTest() {
+		
+		LoginPage login = new LoginPage(driver);
+		login.LoginFunction("real@abc.com","RealPwd@1234");
+
+		
+		
+	}
+
+}
